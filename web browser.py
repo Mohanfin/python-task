bce8664132e938ef24eeeefa494f69ac16f94af7
mohanraj raj <mohamohan03@gmail.com>
2026-07-22 13:36:47 +0530

    Add files via upload

---
+++ web browser.py
@@ -0,0 +1,24 @@
+import webbrowser
+
+# 1. Open URL
+webbrowser.open("https://www.hackerrank.com/")
+
+ # 2. Open in New Window
+webbrowser.open_new("https://www.python.org")
+#
+# # 3. Open in New Tab
+# webbrowser.open_new_tab("https://www.youtube.com")
+#
+# # 4. Get Browser Controller
+# browser = webbrowser.get()
+#
+# # 5. Open URL using Browser Controller
+# browser.open("https://www.github.com")
+#
+# # 6. Open URL in New Window using Controller
+# browser.open_new("https://www.wikipedia.org")
+#
+# # 7. Open URL in New Tab using Controller
+# browser.open_new_tab("https://www.stackoverflow.com")
+#
+# print("All webbrowser functions executed successfully!")
